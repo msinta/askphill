4978f015dc99c86564c3bfb8958364fae62c8e2d
mohamed shegow <shegowmo@gmail.com>
2023-01-10 14:41:44 +0100

    accordian react struture addded

---
+++ src/components/accordion.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+function Accordion() {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const accordionData = [
+    { title: "Daily", content: [ "Sport Socks", "Check"]},
+    { title: "Sports", content: "content 2"},
+    { title: "Travel", content: "content 2"},
+    { title: "Medical", content: "content 2"},
+    { title: "More", content: "content 2"}
+  ]
+
+  const handleAccordionClick = (index) => {
+    if (index === activeIndex) {
+      setActiveIndex(null);
+    } else {
+      setActiveIndex(index);
+    }
+  };
+
+  return (
+    <div>
+      {accordionData.map((item, index) => (
+        <div key={index}>
+          <button onClick={() => handleAccordionClick(index)}>
+            {item.title}
+          </button>
+          {activeIndex === index && <div>{item.content}</div>}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Accordion;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './styles/main.css';
+import Accordion from './components/accordion';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Accordion />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
